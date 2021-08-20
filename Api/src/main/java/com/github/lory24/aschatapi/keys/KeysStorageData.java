9ae2dcf20f786bf0345774ba76f474e5f52ffd8a
LoRy24 <lorytherock24tutorial@gmail.com>
2021-08-20 23:41:36 +0200

    Added new keys features

--- Api/src/main/java/com/github/lory24/aschatapi/keys/KeysStorageData.java
+++ Api/src/main/java/com/github/lory24/aschatapi/keys/KeysStorageData.java
@@ -3,12 +3,8 @@ package com.github.lory24.aschatapi.keys;
 import lombok.Getter;
 import lombok.SneakyThrows;
 
-import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 public class KeysStorageData {
     @Getter private final PublicKey publicKey;
@@ -16,12 +12,7 @@ public class KeysStorageData {
 
     @SneakyThrows
     public KeysStorageData(byte[] publicKeyBytes, byte[] privateKeyBytes) {
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-        this.publicKey = keyFactory.generatePublic(publicKeySpec);
-        EncodedKeySpec privateKeySpec =
-                new PKCS8EncodedKeySpec(privateKeyBytes);
-        this.privateKey = keyFactory.generatePrivate(privateKeySpec);
+        this.publicKey = KeysUtils.publicKeyFromBytes(publicKeyBytes);
+        this.privateKey = KeysUtils.privateKeyFromBytes(privateKeyBytes);
     }
 }
--- Api/src/main/java/com/github/lory24/aschatapi/keys/KeysUtils.java
+++ Api/src/main/java/com/github/lory24/aschatapi/keys/KeysUtils.java
@@ -4,8 +4,12 @@ import lombok.SneakyThrows;
 
 import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class KeysUtils {
 
@@ -22,5 +26,19 @@ public class KeysUtils {
         cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] result = cipher.doFinal(bytes);
         return new String(result);
+    }
+
+    @SneakyThrows
+    public static PublicKey publicKeyFromBytes(byte[] bs) {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(bs);
+        return keyFactory.generatePublic(publicKeySpec);
+    }
+
+    @SneakyThrows
+    public static PrivateKey privateKeyFromBytes(byte[] bs) {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(bs);
+        return keyFactory.generatePrivate(privateKeySpec);
     }
 }
