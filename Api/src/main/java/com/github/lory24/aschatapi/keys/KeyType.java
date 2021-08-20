2ccfd15a9049a2f5ee05e5483ffe7432eb987f80
LoRy24 <51451299+LoryTheRock24@users.noreply.github.com>
2021-08-20 12:01:56 +0200

    Updated KeyData.java features

--- Api/src/main/java/com/github/lory24/aschatapi/keys/KeyType.java
+++
@@ -1,6 +0,0 @@
-package com.github.lory24.aschatapi.keys;
-
-public enum KeyType {
-    PUBLIC,
-    PRIVATE
-}
--- Api/src/main/java/com/github/lory24/aschatapi/storage/KeysData.java
+++ Api/src/main/java/com/github/lory24/aschatapi/storage/KeysData.java
@@ -1,9 +1,14 @@
 package com.github.lory24.aschatapi.storage;
 
-import com.github.lory24.aschatapi.keys.KeysIdentity;
+import com.google.gson.Gson;
 import lombok.SneakyThrows;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class KeysData {
@@ -15,11 +20,30 @@ public class KeysData {
 
     @SneakyThrows
     public void setupFile() {
-        if (!file.exists()) file.createNewFile();
+        if (!file.exists()) {
+            file.createNewFile();
 
+            // generate the keys
+            SecureRandom secureRandom = new SecureRandom();
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048, secureRandom);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            System.out.println("Generated public key: " + keyPair.getPublic().toString());
+
+            Gson gson = new Gson();
+            String keyPairJson = gson.toJson(keyPair);
+            System.out.println(keyPairJson);
+
+            FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
+            fileWriter.write(keyPairJson);
+            fileWriter.close();
+        }
     }
 
-    public KeysIdentity readKeys() {
-        return null;
+    @SneakyThrows
+    public KeyPair readKeys() {
+        String fileContent = Files.readString(Paths.get(file.getAbsolutePath()));
+        Gson gson = new Gson();
+        return gson.fromJson(fileContent, KeyPair.class);
     }
 }
